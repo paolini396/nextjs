770e820eb0b0091bf87170a852c5dc51a9bf60b2
Gabriel Paolini <paolini396@gmail.com>
2021-01-06 20:44:30 -0300

    creating project

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import { GetServerSideProps } from 'next';
+import { Title } from '../styles/pages/Home';
+
+interface iProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: iProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+
+  return (
+    <div>
+
+     <section>
+     <Title>Products</Title>
+
+     <ul>
+     {recommendedProducts.map(recommendedProduct => (
+       <li key={recommendedProduct.id}>
+         {recommendedProduct.title}
+       </li>
+     ))}
+     </ul>
+     </section>
+    </div>
+  )
+}
+
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+}
